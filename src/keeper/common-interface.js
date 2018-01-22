3581e2074e9c0f388d01a03aace1859c2ec23121
Hao Zhang <zh970205@outlook.com>
2018-01-22 21:38:41 +0800

    there-is-a-bug

--- src/keeper/common-interface.js
+++ src/keeper/common-interface.js
@@ -2,19 +2,21 @@ var edgeSize = 128;
 
 class TimeLine{
   constructor(){
-    this.data = Array(edgeSize*edgeSize*2);
+    this.data = [];//Array(edgeSize*edgeSize*2);
     this.top = 0;
   }
-  __addPoint(point, color, time){
-    this.data[this.top] = {x:point.x, y:point.y, r:color.r, g:color.g, b:color.b, t:time, abandoned:false};
+  async __addPoint(point, color, time){
+    this.dataModified = [];
+    this.data.push({x:point.x, y:point.y, r:color.r, g:color.g, b:color.b, t:time, abandoned:false});
     for(var i=0;i<this.top;i++){
       if(this.data[i] && this.data[i].x==point.x && this.data[i].y==point.y){
         this.data[i] = {abandoned: true};
+        this.dataModified.push(i);
       }
     }
     this.top++;
   }
-  __query(time){
+  async __query(time){
     var res = [];
     for(var i=this.top-1;i>=0;i--){
       if(this.data[i].abandoned){
@@ -27,15 +29,19 @@ class TimeLine{
     }
     return res
   }
-  fresh(){
+  async fresh(){
     var newTop=0;
+    var tmpData = [];
     for(var oldTop=0;oldTop<this.top;oldTop++){
       if(this.data[oldTop].abandoned){
+        delete this.data[oldTop]
         continue;
       }
-      this.data[newTop++] = this.data[oldTop];
+      tmpdata[newTop++] = this.data[oldTop];
     }
     this.top = newTop;
+    delete this.data;
+    this.data = tmpData
   }
 }
 
@@ -51,72 +57,84 @@ class TreeNode extends TimeLine{
     this.rightSon = null;
     this.father = father;
     this.flag = (x2 - x1)==edgeSize-1 && (y2 - y1)==edgeSize-1 // True if it is unit
+    this.collection = null;
+  }
+  async init(n){
   }
   whetherInclude(point){
     return this.x1<=point.x && point.x<=this.x2 && this.y1<=point.y && point.y<=this.y2;
   }
-  split(){// not called by leaf
+  async split(){// not called by leaf
     if(this.flag){
       return;
     }
+    this.notSplited = this.leftSon && this.rightSon;
     var x1 = this.x1, x2 = this.x2, y1 = this.y1, y2 = this.y2;
     if((x2-x1)==(y2-y1)){//  True: -- False: --
-      this.leftSon = this.leftSon || new TreeNode(x1, y1, x2, (y1+y2+1)/2-1, this);
-      this.rightSon = this.rightSon || new TreeNode(x1, (y1+y2+1)/2, x2, y2, this);
+      this.leftSon = this.leftSon || new this.constructor(x1, y1, x2, (y1+y2+1)/2-1, this);
+      this.rightSon = this.rightSon || new this.constructor(x1, (y1+y2+1)/2, x2, y2, this);
     }else{
-      this.leftSon = this.leftSon || new TreeNode(x1, y1, (x1+x2+1)/2-1, y2, this);
-      this.rightSon = this.rightSon || new TreeNode((x1+x2+1)/2, y1, x2, y2, this);
+      this.leftSon = this.leftSon || new this.constructor(x1, y1, (x1+x2+1)/2-1, y2, this);
+      this.rightSon = this.rightSon || new this.constructor((x1+x2+1)/2, y1, x2, y2, this);
     }
+    await this.father.init(this.collection);
+    await this.father.rightSon.init(this.collection)
   }
-  extend(){//only called by root node
+  async extend(){//only called by root node
     if(this.father){
       return;
     }
+    this.notExtended = false
     var x1 = this.x1, x2 = this.x2, y1 = this.y1, y2 = this.y2;
     if((x2-x1)==(y2-y1)){
       if(x2+x1>0){
         //left;
-        this.father = new TreeNode(2*x1-x2-1, y1, x2, y2, null);
+        this.father = new this.constructor(2*x1-x2-1, y1, x2, y2, null);
         this.father.leftSon = this;
+        this.father.rightSon = new this.constructor(2*x1-x2-1, y1, x2-1, y2, this.father);
       }else{
         //right
-        this.father = new TreeNode(x1, y1, 2*x2-x1+1, y2, null);
+        this.father = new this.constructor(x1, y1, 2*x2-x1+1, y2, null);
         this.father.leftSon = this;
+        this.father.rightSon = new this.constructor(x1+1, y1, 2*x2-x1+1, y2, this.father);
       }
     }else{
       if(y2+y1>0){
         //bottom
-        this.father = new TreeNode(x1, 2*y1-y2-1, x2, y2, null);
+        this.father = new this.constructor(x1, 2*y1-y2-1, x2, y2, null);
         this.father.leftSon = this;
+        this.father.rightSon = new this.constructor(x1, 2*y1-y2-1, x2, y2-1, this.father);
       }else{
         //top
-        this.father = new TreeNode(x1, y1, x2, 2*y2-y1+1, null);
+        this.father = new this.constructor(x1, y1, x2, 2*y2-y1+1, null);
         this.father.leftSon = this;
+        this.father.rightSon = new this.constructor(x1, y1+1, x2, 2*y2-y1+1, this.father);
       }
     }
-    this.father.split();
+    await this.father.init(this.collection)
+    await this.father.rightSon.init(this.collection)
   }
-  _addPoint(point, color, time){
+  async _addPoint(point, color, time){
     if(this.flag){
-      this.__addPoint(point, color, time);
+      await this.__addPoint(point, color, time);
     }else{
-      this.split();
+      await this.split();
       if(this.leftSon.whetherInclude(point)){
-        this.leftSon._addPoint(point, color, time);
+        await this.leftSon._addPoint(point, color, time);
       }else{
-        this.rightSon._addPoint(point, color, time);
+        await this.rightSon._addPoint(point, color, time);
       }
     }
   }
-  addPoint(point, color, time){//only called by root node
+  async addPoint(point, color, time){//only called by root node
     if(this.father){
-      this.father.addPoint();
+      await this.father.addPoint();
     }else{
       if(this.whetherInclude(point)){
-        this._addPoint(point, color, time);
+        await this._addPoint(point, color, time);
       }else{
-        this.extend();
-        this.father._addPoint(point, color, time);
+        await this.extend();
+        await this.father._addPoint(point, color, time);
       }
     }
   }
@@ -128,34 +146,34 @@ class TreeNode extends TimeLine{
     flag |= this.whetherInclude({x:x2, y:y2});
     return flag;
   }
-  query(x1, y1, x2, y2, time){//only called by root node
+  async query(x1, y1, x2, y2, time){//only called by root node
     if(this.father){
-      return this.father.query(x1, y1, x2, y2, time);
+      return await this.father.query(x1, y1, x2, y2, time);
     }else{
-      return this._query(x1, y1, x2, y2, time);
+      return await this._query(x1, y1, x2, y2, time);
     }
   }
-  _query(x1, y1, x2, y2, time){
+  async _query(x1, y1, x2, y2, time){
     if(!this.whetherOverlap(x1, y1, x2, y2)){//不在的话
       return [];
     }else if(this.flag){//子叶的话
-      return this.__query(time);
+      return await this.__query(time);
     }else{
-      return this.leftSon._query(x1 ,y1, x2, y2, time).concat(
-        this.rightSon._query(x1 ,y1, x2, y2, time));
+      return (await this.leftSon._query(x1 ,y1, x2, y2, time)).concat(
+        await this.rightSon._query(x1 ,y1, x2, y2, time));
     }
   }
-  findAncestor(){
+  async findAncestor(){
     if(!this.father){
       return this;
     }else{
-      return this.father.findAncestor();
+      return await this.father.findAncestor();
     }
   }
 }
 
-function createRoot(){
-  return new TreeNode(-edgeSize/2,-edgeSize/2,edgeSize/2-1,edgeSize/2-1,null);
+function createRoot(type){
+  return new type(-edgeSize/2,-edgeSize/2,edgeSize/2-1,edgeSize/2-1,null);
 }
 
 module.exports={
--- src/keeper/mongodb-interface.js
+++ src/keeper/mongodb-interface.js
@@ -1,5 +1,4 @@
 var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
 var commonInterface = require("./common-interface.js");
 
 var url = 'mongodb://localhost:27017';
@@ -8,12 +7,132 @@ var dbName = 'pixelField';
 async function getCollection(){
   var client = await MongoClient.connect(url);
   var db = client.db(dbName);
-  var collection = db.collection('a');
+  var collection = db.collection('tree');
   return [collection,async ()=>await client.close()]
   //var res = await collection.insertMany([{'a':4}]);
 }
 
-getCollection().then((res)=>{
-  console.log(res)
-  process.exit();
+class MongodbTreeNode extends commonInterface.TreeNode{
+  constructor(x1, y1, x2, y2, father){
+    super(x1, y1, x2, y2, father);
+  }
+  async init(col){
+    console.log(this.x1)
+    this.collection = col;
+    var res = await this.collection.insert({
+      x1:this.x1,
+      y1:this.y1,
+      x2:this.x2,
+      y2:this.y2,
+      flag:this.flag,
+      data:[],
+      top:this.top,
+      father:"",
+      leftSon:"",
+      rightSon:""
+    });
+    this.id = res.ops[0]._id;
+  }
+  getId(node){
+    if(node){
+      return node.id
+    }else{
+      return ""
+    }
+  }
+  async save_node(){
+    //fresh father and son
+    await this.collection.update({_id:this.id},{
+      $set:{
+        father:this.getId(this.father),
+        leftSon:this.getId(this.leftSon),
+        rightSon:this.getId(this.rightSon)
+      }
+    })
+  }
+  timeLinePointConverter(i, notFresh=true){
+    if(this.data[i].abandoned && notFresh){
+      return {abandoned:true}
+    }else{
+      return{
+        x:this.data[i].x,
+        y:this.data[i].y,
+        r:this.data[i].r,
+        g:this.data[i].g,
+        b:this.data[i].b,
+        t:this.data[i].t,
+        abandoned:this.data[i].abandoned
+      }
+    }
+  }
+  async save_point(){
+    //read dataModified
+    if(this.dataModified.length!=0){
+      var setter = {};
+      for(var i of this.dataModified){
+        setter[`data.${i}`] = this.timeLinePointConverter(i);
+      }
+      this.dataModified = [];
+      await this.collection.update({_id:this.id},{
+        $set: setter
+      });
+    }
+    await this.collection.update({_id:this.id},{
+      $push: {
+        data: this.timeLinePointConverter(this.top-1)
+      }
+    })
+  }
+  async save_fresh(){
+    //fresh all data
+    data = [];
+    for(var i of this.dataModified){
+      data.push(timeLinePointConverter(i,false));
+    }
+    await this.collection.update({_id:this.id},{
+      $set: {
+        data
+      }
+    })
+  }
+  async fresh(){
+    super.fresh();
+    await this.save_fresh();
+  }
+  async __addPoint(point, color, time){
+    super.__addPoint(point, color, time);
+    await this.save_point();
+  }
+  async split(){
+    super.split()
+    if(!this.notSplited){
+      this.save_node();
+      this.notSplited = true;
+    }
+  }
+  async extend(){
+    super.extend()
+    if(!this.notExtended){
+      this.father.save_node();
+      this.save_node();
+      this.notExtended = true;
+    }
+  }
+}
+
+async function test(){
+  var c = await getCollection();
+  var t = commonInterface.createRoot(MongodbTreeNode);
+  await t.init(c[0])
+  //await t.addPoint({x:0,y:0},{r:1,g:0,b:0},1)
+  //await t.addPoint({x:2,y:0},{r:1,g:0,b:0},2)
+  //await t.addPoint({x:2,y:0},{r:1,g:0,b:0},4)
+  await t.addPoint({x:0,y:100},{r:2,g:0,b:0},2)
+  await c[1]()
+}
+
+test().then((res)=>{
+  process.exit(0);
+}).catch((res)=>{
+  process.exit(1)
 })
