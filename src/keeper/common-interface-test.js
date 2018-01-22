1347e28795e2a37642374c5a0f880ef4742e8d6e
Hao Zhang <zh970205@outlook.com>
2018-01-22 17:29:56 +0800

    common-interface-test-arranged

--- src/keeper/common-interface-test.js
+++ src/keeper/common-interface-test.js
@@ -1,5 +1,6 @@
 var mod = require("./common-interface.js")
-tree = mod.treeRoot
+tree = mod.createRoot()
 tree.addPoint({x:0,y:0},{r:0,g:0,b:0},0)
 tree.addPoint({x:0,y:100},{r:0,g:0,b:0},0)
-console.log(tree.father.rightSon)
+tree = tree.findAncestor()
+console.log(tree)
