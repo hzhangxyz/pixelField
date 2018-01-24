41fb77efce18c6dc7a10b0036857d99332369f80
Hao Zhang <zh970205@outlook.com>
2018-01-25 01:53:12 +0800

    用于debug的脚本

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ echo "require('babel-polyfill')" > ./tmp/_index.js
 cat ./common-interface.js >>./tmp/_common-interface.js
 cat ./mongodb-interface.js >>./tmp/_mongodb-interface.js
 cat ./index.js >>./tmp/_index.js
-node ./node_modules/babel-cli/bin/babel.js ./tmp/_common-interface.js -o ./tmp/common-interface.js
-node ./node_modules/babel-cli/bin/babel.js ./tmp/_mongodb-interface.js -o ./tmp/mongodb-interface.js
-node ./node_modules/babel-cli/bin/babel.js ./tmp/_index.js -o ./tmp/index.js
-node ./node_modules/babel-cli/bin/babel.js common-interface.js local-storage-interface.js render.js -o ./tmp/compressed.js;
+node ./node_modules/babel-cli/bin/babel.js ./tmp/_common-interface.js -o ./tmp/common-interface.js $DEBUG
+node ./node_modules/babel-cli/bin/babel.js ./tmp/_mongodb-interface.js -o ./tmp/mongodb-interface.js $DEBUG
+node ./node_modules/babel-cli/bin/babel.js ./tmp/_index.js -o ./tmp/index.js $DEBUG
+node ./node_modules/babel-cli/bin/babel.js common-interface.js local-storage-interface.js render.js -o ./tmp/compressed.js $DEBUG
 cat index.html ./tmp/compressed.js >./tmp/index.html;
 echo '</script>' >> ./tmp/index.html
