6f64eee67f4c45672e180b8a79965c6261c0beec
Hao Zhang <zh970205@outlook.com>
2018-01-25 02:22:02 +0800

    标题与node和nodejs的判断

--- build.sh
+++ build.sh
@@ -1,12 +1,19 @@
+if [ `which nodejs 2>/dev/null` ];
+then
+  NODE=nodejs
+else
+  NODE=node
+fi
+
 echo "require('babel-polyfill')" > ./tmp/_common-interface.js
 echo "require('babel-polyfill')" > ./tmp/_mongodb-interface.js
 echo "require('babel-polyfill')" > ./tmp/_index.js
 cat ./common-interface.js >>./tmp/_common-interface.js
 cat ./mongodb-interface.js >>./tmp/_mongodb-interface.js
 cat ./index.js >>./tmp/_index.js
-node ./node_modules/babel-cli/bin/babel.js ./tmp/_common-interface.js -o ./tmp/common-interface.js $DEBUG
-node ./node_modules/babel-cli/bin/babel.js ./tmp/_mongodb-interface.js -o ./tmp/mongodb-interface.js $DEBUG
-node ./node_modules/babel-cli/bin/babel.js ./tmp/_index.js -o ./tmp/index.js $DEBUG
-node ./node_modules/babel-cli/bin/babel.js common-interface.js local-storage-interface.js render.js -o ./tmp/compressed.js $DEBUG
+$NODE ./node_modules/babel-cli/bin/babel.js ./tmp/_common-interface.js -o ./tmp/common-interface.js $DEBUG
+$NODE ./node_modules/babel-cli/bin/babel.js ./tmp/_mongodb-interface.js -o ./tmp/mongodb-interface.js $DEBUG
+$NODE ./node_modules/babel-cli/bin/babel.js ./tmp/_index.js -o ./tmp/index.js $DEBUG
+$NODE ./node_modules/babel-cli/bin/babel.js common-interface.js local-storage-interface.js render.js -o ./tmp/compressed.js $DEBUG
 cat index.html ./tmp/compressed.js >./tmp/index.html;
 echo '</script>' >> ./tmp/index.html
