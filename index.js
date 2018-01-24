c6b036530ff84f4c9a8012cc1945ec68be170c2e
Hao Zhang <zh970205@outlook.com>
2018-01-24 19:21:23 +0800

    初步连接成功

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
-const express = require('express')
-const app = express()
+var keeper = require('./mongodb-interface.js')
+var express = require('express')
+var app = express()
+var expressWs = require('express-ws')(app);
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//var bodyParser = require('body-parser');
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({extended:true}));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+(async ()=>{
+  var c = await keeper.getCollection();
+  var tree = await keeper.recovery(c[0]);
+  if(!tree){
+    tree = keeper.createTree(keeper.MongodbTreeNode, c[0]);
+  }
+  return [tree, c[1]]
+})().then((res)=>{
+
+  var tree = res[0];
+  process.on('exit',res[1])
+
+  app.get('/', express.static('.'))
+  app.get('/common-interface.js', express.static('.'))
+  app.get('/local-storage-interface.js', express.static('.'))
+
+  app.ws('/', function(ws, req) {
+    ws.on('message', function(msg) {
+      try{
+        var data = JSON.parse(msg);
+        if(typeof data.time != 'undefined'){//Query
+          tree.query(data.x1,data.y1,data.x2,data.y2,data.time).then((res)=>{
+            ws.send(JSON.stringify(res))
+          })
+        }else{
+          for(var i of data){
+            tree.addPoint(i[0],i[1],i[2])
+            console.log(i)
+          }
+        }
+      }catch(e){
+        console.log(e)
+      }
+    });
+  });
+  app.listen(3000, () => console.log('Example app listening on port 3000!'))
+})
--- mongodb-interface-test.js
+++ mongodb-interface-test.js
@@ -3,13 +3,13 @@ var mongodbInterface = require("./mongodb-interface.js");
 async function test(){
   var c = await mongodbInterface.getCollection();
   var t = mongodbInterface.createTree(mongodbInterface.MongodbTreeNode, c[0]);
-  await t.addPoint({x:0,y:0},{r:1,g:0,b:0},1)
-  await t.addPoint({x:0,y:1},{r:2,g:0,b:0},2)
-  await t.addPoint({x:0,y:1},{r:3,g:0,b:0},3)
-  await t.addPoint({x:0,y:100},{r:2,g:0,b:0},4)
+  await t.addPoint({x:0,y:0},{r:100,g:0,b:0},1)
+  await t.addPoint({x:0,y:1},{r:2,g:100,b:0},2)
+  await t.addPoint({x:0,y:1},{r:3,g:0,b:100},3)
+  await t.addPoint({x:0,y:100},{r:200,g:100,b:0},4)
   await t.disinit()
-  await t.addPoint({x:0,y:1},{r:3,g:0,b:0},3)
-  await t.addPoint({x:0,y:100},{r:5,g:0,b:0},5)
+  await t.addPoint({x:0,y:1},{r:100,g:200,b:150},3)
+  await t.addPoint({x:0,y:100},{r:250,g:200,b:150},5)
   await t.freshAll()
   //console.log(t)
   for(var i of await c[0].find({}).toArray()){
