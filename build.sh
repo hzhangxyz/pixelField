a3108a15381251849c11fd8ef2ddcf59519caee0
Hao Zhang <zh970205@outlook.com>
2018-01-25 00:34:09 +0800

    emmm

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
-echo "require('babel-polyfill')" > ./tmp/common-interface.js
-echo "require('babel-polyfill')" > ./tmp/mongodb-interface.js
-echo "require('babel-polyfill')" > ./tmp/index.js
-cat ./common-interface.js >>./tmp/common-interface.js
-cat ./mongodb-interface.js >>./tmp/mongodb-interface.js
-cat ./index.js >>./tmp/index.js
-node ./node_modules/babel-cli/bin/babel.js ./tmp/common-interface.js ./tmp/mongodb-interface.js ./tmp/index.js -d tmp
+echo "require('babel-polyfill')" > ./tmp/_common-interface.js
+echo "require('babel-polyfill')" > ./tmp/_mongodb-interface.js
+echo "require('babel-polyfill')" > ./tmp/_index.js
+cat ./common-interface.js >>./tmp/_common-interface.js
+cat ./mongodb-interface.js >>./tmp/_mongodb-interface.js
+cat ./index.js >>./tmp/_index.js
+node ./node_modules/babel-cli/bin/babel.js ./tmp/_common-interface.js -o ./tmp/common-interface.js
+node ./node_modules/babel-cli/bin/babel.js ./tmp/_mongodb-interface.js -o ./tmp/mongodb-interface.js
+node ./node_modules/babel-cli/bin/babel.js ./tmp/_index.js -o ./tmp/index.js
 node ./node_modules/babel-cli/bin/babel.js common-interface.js local-storage-interface.js render.js -o ./tmp/compressed.js;
 cat index.html ./tmp/compressed.js >./tmp/index.html;
 echo '</script>' >> ./tmp/index.html
