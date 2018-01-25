e50a917d93be8df7796beaffc69faee7a698fa24
Hao Zhang <zh970205@outlook.com>
2018-01-26 00:35:47 +0800

    tmp-rename-to-build

--- build.sh
+++ build.sh
@@ -5,17 +5,17 @@ else
   NODE=node
 fi
 
-echo "require('babel-polyfill')" > ./tmp/_common-interface.js
-echo "require('babel-polyfill')" > ./tmp/_mongodb-interface.js
-echo "require('babel-polyfill')" > ./tmp/_index.js
-cat ./common-interface.js >>./tmp/_common-interface.js
-cat ./mongodb-interface.js >>./tmp/_mongodb-interface.js
-cat ./index.js >>./tmp/_index.js
-$NODE ./node_modules/babel-cli/bin/babel.js ./tmp/_common-interface.js -o ./tmp/common-interface.js $DEBUG
-$NODE ./node_modules/babel-cli/bin/babel.js ./tmp/_mongodb-interface.js -o ./tmp/mongodb-interface.js $DEBUG
-$NODE ./node_modules/babel-cli/bin/babel.js ./tmp/_index.js -o ./tmp/index.js $DEBUG
-$NODE ./node_modules/babel-cli/bin/babel.js common-interface.js local-storage-interface.js render.js -o ./tmp/compressed.js $DEBUG
-expr `cat index.html | wc -l` - 4 | xargs head index.html -n > ./tmp/index.html
-echo '<script>' >> ./tmp/index.html
-cat ./tmp/compressed.js >> ./tmp/index.html;
-echo '</script>' >> ./tmp/index.html
+echo "require('babel-polyfill')" > ./build/_common-interface.js
+echo "require('babel-polyfill')" > ./build/_mongodb-interface.js
+echo "require('babel-polyfill')" > ./build/_index.js
+cat ./common-interface.js >>./build/_common-interface.js
+cat ./mongodb-interface.js >>./build/_mongodb-interface.js
+cat ./index.js >>./build/_index.js
+$NODE ./node_modules/babel-cli/bin/babel.js ./build/_common-interface.js -o ./build/common-interface.js $DEBUG
+$NODE ./node_modules/babel-cli/bin/babel.js ./build/_mongodb-interface.js -o ./build/mongodb-interface.js $DEBUG
+$NODE ./node_modules/babel-cli/bin/babel.js ./build/_index.js -o ./build/index.js $DEBUG
+$NODE ./node_modules/babel-cli/bin/babel.js common-interface.js local-storage-interface.js render.js -o ./build/compressed.js $DEBUG
+expr `cat index.html | wc -l` - 4 | xargs head index.html -n > ./build/index.html
+echo '<script>' >> ./build/index.html
+cat ./build/compressed.js >> ./build/index.html;
+echo '</script>' >> ./build/index.html
