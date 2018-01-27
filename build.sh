bdec87e5779e2810a36bb9752d7b7e65e452f006
Hao Zhang <zh970205@outlook.com>
2018-01-28 00:16:41 +0800

    改一下readme和build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 mkdir -p build
 node ./node_modules/babel-cli/bin/babel.js *.js -d ./build/ $DEBUG ||
   nodejs ./node_modules/babel-cli/bin/babel.js *.js -d ./build/ $DEBUG
-expr `cat index.html | wc -l` - 3 | xargs head index.html -n > ./build/index.html
-echo '<script>' >> ./build/index.html
-cat ./build/front-model.js ./build/front-driver.js >> ./build/index.html;
-echo '</script>' >> ./build/index.html
+cp ./index.html ./build/index.html
