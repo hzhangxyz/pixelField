2ad7ba22a9199bc0fd77ca34cf6d22c4fb03bf04
Hao Zhang <zh970205@outlook.com>
2018-01-27 23:04:19 +0800

    node和nodejs的问题

--- build.sh
+++ build.sh
@@ -1,12 +1,6 @@
-if [ `which nodejs 2>/dev/null` ];
-then
-  NODE=nodejs
-else
-  NODE=node
-fi
-
 mkdir -p build
-$NODE ./node_modules/babel-cli/bin/babel.js *.js -d ./build/ $DEBUG
+node ./node_modules/babel-cli/bin/babel.js *.js -d ./build/ $DEBUG ||
+  nodejs ./node_modules/babel-cli/bin/babel.js *.js -d ./build/ $DEBUG
 expr `cat index.html | wc -l` - 3 | xargs head index.html -n > ./build/index.html
 echo '<script>' >> ./build/index.html
 cat ./build/front-model.js ./build/front-driver.js >> ./build/index.html;
