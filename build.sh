3b5f4f60602d1b57a0a9d849fc5e39715e50af19
Hao Zhang <zh970205@outlook.com>
2018-01-28 15:22:20 +0800

    改一下build.sh..要不版本号改成1.0吧...

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@ mkdir -p build
 node ./node_modules/babel-cli/bin/babel.js *.js -d ./build/ $DEBUG ||
   nodejs ./node_modules/babel-cli/bin/babel.js *.js -d ./build/ $DEBUG
 cp ./index.html ./build/index.html
+cd ./build
+tar zcvf `git rev-parse HEAD`.tar.gz *.js *.html
